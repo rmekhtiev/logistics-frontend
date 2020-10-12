87ccbe32b4024d08a4eae01813afb83078c8d56f
rmekhtiev <mekhtievrs0@gmail.com>
2020-10-12 20:47:26 +0300

    feat: add drawer

---
+++ components/TheDrawer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <template v-for="(item, key) in items">
+          <v-list-item :key="key" :to="key">
+            <v-list-item-avatar>
+              <v-icon>
+                {{ item.icon }}
+              </v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ key }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheDrawer",
+  data: () => ({
+    drawer: null,
+    items: {
+      drivers: {
+        icon: 'mdi-account',
+        to: {
+          name: 'drivers',
+        }
+      }
+    }
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,10 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-email</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      app
-      color="indigo"
-      dark
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
+   <the-drawer />
 
     <v-main style="background-color: rgb(248, 250, 254);">
       <v-container fluid>
-        <nuxt />
+        <nuxt/>
       </v-container>
     </v-main>
     <v-footer
@@ -48,9 +17,8 @@
 </template>
 
 <script>
+import TheDrawer from "@/components/TheDrawer";
 export default {
-  data: () => ({
-    drawer: null,
-  }),
+  components: {TheDrawer},
 }
 </script>
