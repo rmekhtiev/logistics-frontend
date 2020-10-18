1542b713fd4312d776d9b29c6d722fbf8a502cda
rmekhtiev <mekhtievrs0@gmail.com>
2020-10-18 20:36:44 +0300

    feat: add some create buttons to applications

--- pages/applications/_id.vue
+++ pages/applications/_id.vue
@@ -2,7 +2,45 @@
   <div id="application">
     <v-row>
       <v-col sm="12" md="6" lg="4">
-        <application-info-card :application="application"/>
+        <application-info-card class="mb-2" :application="application"/>
+        <v-card class="mb-2" outlined>
+          <v-card-title>
+            <div class="overline"> Получатель</div>
+            <v-spacer/>
+            <v-btn icon color="primary">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-card-title>
+        </v-card>
+        <v-card outlined>
+          <v-card-title>
+            <div class="overline"> Отправитель</div>
+            <v-spacer/>
+            <v-btn icon color="primary">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col sm="12" md="6" lg="4">
+        <v-card class="mb-2" outlined>
+          <v-card-title>
+            <div class="overline">Водитель</div>
+            <v-spacer/>
+            <v-btn icon color="primary">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-card-title>
+        </v-card>
+        <v-card outlined>
+          <v-card-title>
+            <div class="overline">Транспортное средство</div>
+            <v-spacer/>
+            <v-btn icon color="primary">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-card-title>
+        </v-card>
       </v-col>
     </v-row>
   </div>
@@ -22,7 +60,7 @@ export default {
           name: "Доставка железа",
           conclusion_date: '24.04.2020'
         }
-      }
+      },
   }),
 }
 </script>
